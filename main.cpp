bd5b4e5827f4cc33cd0dd9e6f3b7702b1882a2cc
ValentiniD <100331774+ValentiniD@users.noreply.github.com>
2022-03-29 18:41:57 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,35 @@
 using namespace std;
 
 int main() {
-   return 0;
+    float a=0, b=0, triangolo=0, quadrato=0, rettangolo=0;
+    int opzione=0;
+
+    cout << "Inserire due valori reali: " << endl;
+    cin >> a >> b;
+    cout << "\n";
+    cout << "Scegli un valore tra 0, 1 e 2: " << endl;
+    cin >> opzione;
+    cout << "\n";
+
+    switch(opzione){
+        case 0:
+            opzione=0;
+            triangolo=(a*b)/2;
+            cout << "L'area del triangolo e\': " << triangolo << endl;
+            break;
+        case 1:
+            opzione=1;
+            quadrato=a*a;
+            cout << "L'area del quadrato e\': " << quadrato << endl;
+            break;
+        case 2:
+            opzione=2;
+            rettangolo=a*b;
+            cout << "L'area del rettangolo e\': " << rettangolo << endl;
+            break;
+        default:
+            cout << "Il valore inserito non e\' valido!" << endl;
+    }
+
+    return 0;
 }
